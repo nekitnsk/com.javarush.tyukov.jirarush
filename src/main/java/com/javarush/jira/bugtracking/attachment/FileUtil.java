8c3889b022edf6cfb7b91f6258c0d2de81d208b0
Nikita Tyukov <tukov@bk.ru>
2025-06-28 21:43:18 +0700

    Переделан метод Upload на использование nio вместо File Использовано deleteIfExists вместо delete

--- src/main/java/com/javarush/jira/bugtracking/attachment/FileUtil.java
+++ src/main/java/com/javarush/jira/bugtracking/attachment/FileUtil.java
@@ -15,6 +15,7 @@ import java.net.MalformedURLException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
 
 @UtilityClass
 public class FileUtil {
@@ -25,14 +26,14 @@ public class FileUtil {
             throw new IllegalRequestDataException("Select a file to upload.");
         }
 
-        File dir = new File(directoryPath);
-        if (dir.exists() || dir.mkdirs()) {
-            File file = new File(directoryPath + fileName);
-            try (OutputStream outStream = new FileOutputStream(file)) {
-                outStream.write(multipartFile.getBytes());
-            } catch (IOException ex) {
-                throw new IllegalRequestDataException("Failed to upload file" + multipartFile.getOriginalFilename());
-            }
+        Path dirPath = Paths.get(directoryPath);
+        try {
+            Files.createDirectories(dirPath);
+            Path filePath = dirPath.resolve(fileName);
+            Files.copy(multipartFile.getInputStream(), filePath,
+                    StandardCopyOption.REPLACE_EXISTING);
+        } catch (IOException ex) {
+            throw new IllegalRequestDataException("Failed to upload file " + multipartFile.getOriginalFilename());
         }
     }
 
@@ -53,7 +54,7 @@ public class FileUtil {
     public static void delete(String fileLink) {
         Path path = Paths.get(fileLink);
         try {
-            Files.delete(path);
+            Files.deleteIfExists(path);
         } catch (IOException ex) {
             throw new IllegalRequestDataException("File" + fileLink + " deletion failed.");
         }
